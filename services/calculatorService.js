6b9633a0edfd0c7e17fd0443e959c60579275139
pacisnerosi24 <pacisnerosi@uce.edu.ec>
2025-01-13 00:24:51 -0500

    feat: :sparkles: add calculator server file (functios)

---
+++ services/calculatorService.js
@@ -0,0 +1,17 @@
+function add(number1, number2){
+    return number1 + number2;
+}
+
+function subtract(number1, number2){
+    return number1 - number2;
+}
+
+function multiply(number1, number2){
+    return number1 * number2;
+}
+
+module.exports = {
+    add,
+    subtract,
+    multiply,
+};
